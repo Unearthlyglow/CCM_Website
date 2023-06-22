7f0b0c6d330f11f9810a97f9204e8d6d0200f457
Award <awardmalisi@gmail.com>
2023-06-22 11:11:32 -0500

    11:11am update

--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -1,6 +1,12 @@
-<script></script>
+<script>
+	import gsap from 'gsap';
+	// import { onMount } from 'svelte';
+	// gsap.to('.box', { x: 200 });
+</script>
 
 <nav class="container">
+	<div class="box green" />
+
 	<details role="list">
 		<summary class="tabs--single" aria-haspopup="listbox" role="primary">Writing</summary>
 		<ul role="listbox">
@@ -27,10 +33,17 @@
 <style>
 	.container {
 		display: flex;
+		font-weight: 800;
+		align-items: center;
+	}
+
+	summary {
+		color: white;
 	}
 
 	summary:hover {
 		color: rgb(231, 139, 139);
+		background-color: rgba(233, 228, 228, 0.687);
 	}
 
 	details {
