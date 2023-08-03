0ad91e57e7004dfc289efe32c033d6142c354100
Award <awardmalisi@gmail.com>
2023-08-02 23:48:18 -0500

    config file update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +11,5 @@ const config = {
 };
 
 export default {
-	config,
-	preprocess: [vitePreprocess()]
+	config
 };
