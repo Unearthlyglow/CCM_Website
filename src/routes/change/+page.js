b9820754a11ffee454b600c8b67d7cc48c999591
Award <awardmalisi@gmail.com>
2023-09-05 13:24:13 -0500

    deployment debugging

--- src/routes/change/+page.js
+++
@@ -1,8 +0,0 @@
-export function load() {
-	return {
-		page: {
-			title: `Blank Page`,
-			description: 'Page Created For Testing/Design Work'
-		}
-	};
-}
--- src/routes/change/+page.svelte
+++
@@ -1,7 +0,0 @@
-<div>Blank Page</div>
-
-<style>
-	div {
-		margin: 40rem;
-	}
-</style>
