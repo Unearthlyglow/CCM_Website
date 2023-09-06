7f98c3444a54fd890746c393d096dea218434549
Award <awardmalisi@gmail.com>
2023-09-06 02:07:29 -0500

    local

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4addc9b0.js","_app/immutable/chunks/index.e1f5af94.js","_app/immutable/chunks/stores.f94b87fc.js","_app/immutable/chunks/singletons.db38794c.js"];
+export const imports = ["_app/immutable/nodes/1.9fca47c9.js","_app/immutable/chunks/index.e1f5af94.js","_app/immutable/chunks/stores.152721de.js","_app/immutable/chunks/singletons.8d09926d.js"];
 export const stylesheets = ["_app/immutable/assets/1.9a8a24fd.css"];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set([".DS_Store","favicon.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.eefc3cf5.js","app":"_app/immutable/entry/app.0dd8df6c.js","imports":["_app/immutable/entry/start.eefc3cf5.js","_app/immutable/chunks/index.e1f5af94.js","_app/immutable/chunks/singletons.db38794c.js","_app/immutable/entry/app.0dd8df6c.js","_app/immutable/chunks/index.e1f5af94.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.3b3fcc50.js","app":"_app/immutable/entry/app.5ee87f67.js","imports":["_app/immutable/entry/start.3b3fcc50.js","_app/immutable/chunks/index.e1f5af94.js","_app/immutable/chunks/singletons.8d09926d.js","_app/immutable/entry/app.5ee87f67.js","_app/immutable/chunks/index.e1f5af94.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('../output/server/nodes/0.js'),
 			() => import('../output/server/nodes/1.js')
