a0eb338422be60ad9258fa68378e807ab74f154b
Award <awardmalisi@gmail.com>
2023-09-06 23:49:44 -0500

    css issues

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.js
+++
@@ -1,6 +0,0 @@
-import { inject } from "@vercel/analytics";
-const prerender = true;
-inject({ mode: "production" });
-export {
-  prerender
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.3011fe4c.js","_app/immutable/chunks/index.e1f5af94.js","_app/immutable/chunks/stores.406c49b2.js","_app/immutable/chunks/singletons.763ade29.js"];
-export const stylesheets = ["_app/immutable/assets/1.9a8a24fd.css"];
-export const fonts = [];
